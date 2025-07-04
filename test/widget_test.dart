f38c7d54994d28d1ff94f2b7e753d577208d4331
Guspascad420 <Paacad@gmail.com>
2025-07-04 21:35:30 +0700

    Initialized app with loggedIn status for widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:hematologi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(initialStatus: AuthStatus.loggedIn));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
