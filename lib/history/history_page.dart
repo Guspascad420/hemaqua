656db06a1b52a132ae611f29a6bdd1304a27ddb4
Guspascad420 <Paacad@gmail.com>
2025-07-04 22:12:50 +0700

    Added water quality gauge and improved UI

--- lib/history/history_page.dart
+++ lib/history/history_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hematologi/components/analysis_result_card.dart';
+import 'package:hematologi/components/analysis/analysis_result_card.dart';
 import 'package:hematologi/hematologi/hematologi_results.dart';
 import 'package:hematologi/hemosit/hemosit_results.dart';
 import 'package:hematologi/history/history_filter_panel.dart';
