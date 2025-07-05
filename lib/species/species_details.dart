c874924f645cb2cfedc5fc935deadbab94febaac
Guspascad420 <Paacad@gmail.com>
2025-07-05 12:39:15 +0700

    Refactored species details for total analysis count

--- lib/species/species_details.dart
+++ lib/species/species_details.dart
@@ -54,6 +54,121 @@ class _SpeciesDetailsState extends ConsumerState<SpeciesDetails> {
     _showSnackBar(context, "Berhasil menghapus ikan dari favorit", Colors.blue);
   }
 
+  Widget _mainContent(int totalAnalysisCount) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.only(left: 20),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(right: 20),
+                width: 70,
+                height: 10,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey),
+                ),
+              ),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.species.name,
+                    style: GoogleFonts.poppins(
+                        color: Colors.blue,
+                        fontSize: 27,
+                        fontWeight: FontWeight.w500
+                    )),
+                Visibility(
+                    visible: ref.read(authStateProvider).asData?.value?.uid != null,
+                    child: Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        child: IconButton(
+                            onPressed: _isFavorite ? _removeSpeciesFromFavorite : _addSpeciesToFavorite,
+                            icon: _isFavorite
+                                ? const Icon(Icons.favorite, color: Colors.red)
+                                : const Icon(Icons.favorite_border)
+                        )
+                    )
+                )
+              ],
+            ),
+            Text(widget.species.latin_name,
+                style: GoogleFonts.poppins(
+                    color: Colors.grey,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500
+                )),
+            const SizedBox(height: 25),
+            Row(
+              children: [
+                const Icon(Icons.location_on, color: Colors.blue, size: 40,),
+                const SizedBox(width: 4),
+                Text('Stasiun ${widget.station}',
+                    style: GoogleFonts.poppins(
+                        color: Colors.grey,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500
+                    )),
+              ],
+            ),
+            const SizedBox(height: 25),
+            Text('Deskripsi',
+                style: GoogleFonts.poppins(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500
+                )),
+            const SizedBox(height: 5),
+            Text(widget.species.description,
+                style: GoogleFonts.poppins(
+                  color: Colors.grey,
+                  fontSize: 15,
+                )),
+            SizedBox(height: 15.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Total Analisis',
+                    style: GoogleFonts.poppins(
+                        color: Colors.blue,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500
+                    )),
+                Container(
+                  margin: EdgeInsets.only(right: 15.w),
+                  child: Text('$totalAnalysisCount Kali',
+                      style: GoogleFonts.poppins(
+                          fontSize: 18,
+                          color: Colors.grey
+                      )),
+                )
+              ],
+            ),
+            SizedBox(height: 15.h),
+            Text('Tren Parameter',
+                style: GoogleFonts.poppins(
+                    color: Colors.blue,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500
+                )),
+            SizedBox(height: 15.h),
+
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   void initState() {
     super.initState();
@@ -63,6 +178,7 @@ class _SpeciesDetailsState extends ConsumerState<SpeciesDetails> {
   @override
   Widget build(BuildContext context) {
     final imageUrlAsync = ref.watch(imageUrlProvider(widget.species.image_url ?? ''));
+    final totalAnalysisCount = ref.watch(totalAnalysisCountProvider(widget.species.type));
 
     return Scaffold(
       backgroundColor: Colors.blue,
@@ -131,117 +247,7 @@ class _SpeciesDetailsState extends ConsumerState<SpeciesDetails> {
                             );
                           }
                       ),
-                      Expanded(
-                        child: Container(
-                          padding: const EdgeInsets.only(left: 20),
-                          decoration: const BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(height: 20),
-                              Center(
-                                child: Container(
-                                  margin: const EdgeInsets.only(right: 20),
-                                  width: 70,
-                                  height: 10,
-                                  child: DecoratedBox(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(10),
-                                        color: Colors.grey),
-                                  ),
-                                ),
-                              ),
-                              const SizedBox(height: 30),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(widget.species.name,
-                                      style: GoogleFonts.poppins(
-                                          color: Colors.blue,
-                                          fontSize: 27,
-                                          fontWeight: FontWeight.w500
-                                      )),
-                                  Visibility(
-                                      visible: ref.read(authStateProvider).asData?.value?.uid != null,
-                                      child: Container(
-                                          margin: const EdgeInsets.only(right: 10),
-                                          child: IconButton(
-                                              onPressed: _isFavorite ? _removeSpeciesFromFavorite : _addSpeciesToFavorite,
-                                              icon: _isFavorite
-                                                  ? const Icon(Icons.favorite, color: Colors.red)
-                                                  : const Icon(Icons.favorite_border)
-                                          )
-                                      )
-                                  )
-                                ],
-                              ),
-                              Text(widget.species.latin_name,
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.grey,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500
-                                  )),
-                              const SizedBox(height: 25),
-                              Row(
-                                children: [
-                                  const Icon(Icons.location_on, color: Colors.blue, size: 40,),
-                                  const SizedBox(width: 4),
-                                  Text('Stasiun ${widget.station}',
-                                      style: GoogleFonts.poppins(
-                                          color: Colors.grey,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w500
-                                      )),
-                                ],
-                              ),
-                              const SizedBox(height: 25),
-                              Text('Deskripsi',
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.blue,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500
-                                  )),
-                              const SizedBox(height: 5),
-                              Text(widget.species.description,
-                                  style: GoogleFonts.poppins(
-                                    color: Colors.grey,
-                                    fontSize: 15,
-                                  )),
-                              SizedBox(height: 15.h),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text('Total Analisis',
-                                      style: GoogleFonts.poppins(
-                                          color: Colors.blue,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w500
-                                      )),
-                                  Container(
-                                    margin: EdgeInsets.only(right: 15.w),
-                                    child: Text('0 Kali',
-                                        style: GoogleFonts.poppins(
-                                            fontSize: 18,
-                                            color: Colors.grey
-                                        )),
-                                  )
-                                ],
-                              ),
-                              SizedBox(height: 15.h),
-                              Text('Tren Parameter',
-                                  style: GoogleFonts.poppins(
-                                      color: Colors.blue,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500
-                                  )),
-                              SizedBox(height: 25.h),
-                            ],
-                          ),
-                        ),
-                      )
+                      _mainContent(totalAnalysisCount.value ?? 0)
                     ],
                   )
                 ),
